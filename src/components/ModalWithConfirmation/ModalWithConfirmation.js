c18fe07f1a5d459a807ad6dde862ac764a592b46
Jaxson Tanner <jaxsonumkc@gmail.com>
2023-12-10 17:24:45 -0600

    Project 11 submission 5

--- src/components/ModalWithConfirmation/ModalWithConfirmation.js
+++ src/components/ModalWithConfirmation/ModalWithConfirmation.js
@@ -1,43 +1,27 @@
-import ModalWithForm from "../ModalWithForm/ModalWithForm";
-import "./ModalWithConfirmation.css";
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
+import './ModalWithConfirmation.css';
 
 const ModalWithConfirmation = ({ onClose, isOpen, onSubmit, buttonText }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit();
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onSubmit();
+	};
 
-  return (
-    <ModalWithForm
-      title=""
-      buttonText="Delete item"
-      onClose={onClose}
-      isOpen={isOpen}
-      onSubmit={handleSubmit}
-      className="modal__confirm_deletion"
-      
-    >
-      <p className="modal__text">Are you sure you want to delete this item?</p>
-      <p className="modal__text">This action is irreversible.</p>
-      <div className="modal__confirm_btns">
-        {/* <button
-          onClick={handleSubmit}
-          className="confirm__modal_btn"
-          type="submit"
-        >
-          {buttonText}
-        </button> */}
-        
-        <button
-          onClick={onClose}
-          className="confirm__modal_btn"
-          type="button"
-        >
-          Cancel
-        </button>
-      </div>
-    </ModalWithForm>
-  );
+	return (
+		<ModalWithForm
+			title=""
+			buttonText="Delete item"
+			onClose={onClose}
+			isOpen={isOpen}
+			onSubmit={handleSubmit}
+			className="modal__confirm_deletion"
+      additionalButton={<button onClick={onClose} className="modal__add_btn"> Cancel </button>}
+		>
+			<p className="modal__text">Are you sure you want to delete this item?</p>
+			<p className="modal__text">This action is irreversible.</p>
+			
+		</ModalWithForm>
+	);
 };
 
 export default ModalWithConfirmation;
--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,6 +1,6 @@
 import './ModalWithForm.css'
 
-function ModalWithForm({ children, buttonText = "Add garment", title, onClose, name, onSubmit }) {
+function ModalWithForm({ children, buttonText = "Add garment", title, onClose, name, onSubmit, additionalButton }) {
 
     return (
         <div className={`modal modal__type_${name}`}>
@@ -9,7 +9,11 @@ function ModalWithForm({ children, buttonText = "Add garment", title, onClose, n
                 <h3 className="modal__title">{title}</h3>
                 <form className="modal__add_form" onSubmit={onSubmit}>
                     {children}
-                    <button className="modal__add modal__add_btn" type="submit">{buttonText}</button>
+                    <div className="add_class_with_flex">
+                    <button className="modal__add modal__add_btn" type="submit">{buttonText}
+                    </button>
+                    {additionalButton}
+                    </div>
                 </form>
             </div>
         </div>
